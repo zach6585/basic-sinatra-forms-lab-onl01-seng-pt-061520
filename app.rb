e20d9615d50351c86e87cb0d83a9fd50416b3485
zachary mines <zmines@wesleyan.edu>
2020-09-13 17:47:13 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,20 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
-
-
+  get '/newteam' do 
+    erb :newteam
+  end 
+  
+  post '/newteam' do 
+    @a = []
+    b = ['Team Name','Coach','Point Guard','Shooting Guard','Small Forward', 'Power Forward','Center']
+    i = 0
+    params.each do |k,v| 
+      @a << "#{b[i]}: #{v}"
+      i +=1 
+    end 
+    @a
+    erb :team
+  end 
+  
 end
