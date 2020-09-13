e5b2483c899753b881714a737547d77631cf7a45
zachary mines <zmines@wesleyan.edu>
2020-09-13 17:43:31 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,17 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
-
-
+  get '/newteam' do 
+    erb :newteam
+  end 
+  
+  post '/newteam' do 
+    @a = []
+    params.each do |k,v| 
+      @a << "#{k}: #{v}"
+    end 
+    @a
+    erb :team
+  end 
+  
 end
